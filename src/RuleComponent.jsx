d50837cfb8b0c0e30f477204861e01c153d4316e
ali <ali.lotfdar58@gmail.com>
2024-05-25 22:19:42 -0400

    Added Edit field

--- src/RuleComponent.jsx
+++ src/RuleComponent.jsx
@@ -1,25 +1,47 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import { Box, Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'rule',
-    headerName: 'Rule',
-    width: 150,
-    editable: true,
-  }
-];
-
-const rows = [
-  { id: 1, rule: 'rule1' },
-  { id: 2, rule: 'rule2' },
-  { id: 3, rule: 'rule3' },
-  { id: 4, rule: 'rule4' },
-];
 
 export default function RuleComponent() {
+
+  const columns = [
+    {
+      field: 'edit',
+      headerName: '',
+      width: 100,
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="primary"
+          onClick={() => handleEditClick(params.id)}
+        >
+          Edit
+        </Button>
+      )
+    },
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'rule',
+      headerName: 'Rule',
+      width: 150,
+      editable: true,
+    }
+  ];
+  
+  const rows = [
+    { id: 1, rule: 'rule1' },
+    { id: 2, rule: 'rule2' },
+    { id: 3, rule: 'rule3' },
+    { id: 4, rule: 'rule4' },
+  ];
+
+  const handleEditClick = (id) => {
+    // Handle the edit button click
+    console.log(`Edit button clicked for row id: ${id}`);
+  };
+
+
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -33,7 +55,6 @@ export default function RuleComponent() {
           },
         }}
         pageSizeOptions={[5]}
-        checkboxSelection
         disableRowSelectionOnClick
       />
     </Box>
